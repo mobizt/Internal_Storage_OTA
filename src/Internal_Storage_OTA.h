dac89654e1bfde8130d2e00b156c993f76de5a3a
unknown <k_suwatchai@hotmail.com>
2024-07-02 10:05:26 +0700

    First commit

---
+++ src/Internal_Storage_OTA.h
@@ -0,0 +1,14 @@
+#ifndef INTERNAL_STORAGE_OTA_H
+#define INTERNAL_STORAGE_OTA_H
+#include <Arduino.h>
+#include "InternalStorage.h"
+
+
+class Internal_Storage_OTA
+{
+public:
+    Internal_Storage_OTA(){}
+    ~Internal_Storage_OTA(){}
+};
+
+#endif
